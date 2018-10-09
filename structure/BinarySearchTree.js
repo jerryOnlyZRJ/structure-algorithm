975a3d8920581a5f263cf778b45bb725a2c2dc25
ranjayzheng <ranjayzheng@tencent.com>
2018-10-09 10:33:14 +0800

    add bst

---
+++ structure/BinarySearchTree.js
@@ -0,0 +1,82 @@
+/**
+ * 二叉排序树节点
+ */
+class BstNode {
+    constructor(data, leftNode, rightNode) {
+        this.data = data
+        this.leftNode = leftNode
+        this.rightNode = rightNode
+    }
+}
+
+/**
+ * 二叉排序树
+ */
+class BST {
+    constructor(dataArr) {
+        this.root = null
+        dataArr && dataArr.map(item => {
+            this.insert(item)
+        })
+    }
+    insert(data) {
+        const newNode = new BstNode(data, null, null)
+        if (!this.root) {
+            this.root = newNode
+        } else {
+            let currentNode = this.root
+            while (currentNode) {
+                if (data < currentNode.data) {
+                    if(currentNode.leftNode){
+                        currentNode = currentNode.leftNode
+                    }else {
+                        currentNode.leftNode = newNode
+                        break
+                    }
+                } else {
+                    if(currentNode.rightNode){
+                        currentNode = currentNode.rightNode
+                    }else {
+                        currentNode.rightNode = newNode
+                        break
+                    }
+                }
+            }
+        }
+    }
+    /**
+     * 前序遍历
+     * @param {BstNode} node 
+     */
+    DLR(node = this.root) {
+        if (node) {
+            console.log(node.data)
+            this.DLR(node.leftNode)
+            this.DLR(node.rightNode)
+        }
+    }
+    /**
+     * 中序遍历
+     * 对于二叉排序树而言，中序遍历就是排序算法
+     * @param {BstNode} node 
+     */
+    LDR(node = this.root) {
+        if (node) {
+            this.LDR(node.leftNode)
+            console.log(node.data)
+            this.LDR(node.rightNode)
+        }
+    }
+    /**
+     * 后序遍历
+     * @param {BstNode} node 
+     */
+    LRD(node = this.root) {
+        if (node) {
+            this.LRD(node.leftNode)
+            this.LRD(node.rightNode)
+            console.log(node.data)
+        }
+    }
+
+}
